a907d8a5ea329d4f5171f288be0d34eb2befd733
Lawrence Lian <LawrensuLeo@gmail.com>
2024-10-25 08:56:04 +0800

    Added header into include folder

---
+++ include/header.php
@@ -0,0 +1,57 @@
+<!-- Header Part -->
+<header id="top" class="header container">
+    <!-- Navigation -->
+    <nav class="nav__wrapper-logo">
+        <a href="index.html"><img class="logo__img" src="images/constant/logo.png" alt="Leafly"></a>
+    </nav>
+
+    <nav class="nav__wrapper-links">
+        <input type="checkbox" id="nav-toggle" class="nav__toggle-checkbox">
+        <label for="nav-toggle" class="nav__toggle-label">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+            
+        <ul class="nav__menu">
+            <li><a class="nav__link" href="classification.html">Classification</a></li>
+
+               <li><a class="nav__link" href="tutorial.html">Tutorial</a>
+                <ul class="dropdown">
+                    <li><a href="tutorial.html">How to do</a></li>
+                    <li><a href="tools.html">Tools to use</a></li>
+                    <li><a href="aftercare.html">Aftercare</a></li>
+                </ul>
+            </li>
+
+            <li><a class="nav__link" href="identification.html">Identification</a></li>
+
+            <li><a class="nav__link" href="contribution.html">Contribution</a></li>
+
+            <li><a class="nav__link" href="enquiry.html">Enquiry</a></li>
+
+            <li><a class="nav__link" href="profile.html">About</a>
+                <ul class="dropdown">
+                    <li><a href="profile.html">Meet the Team</a></li>
+                    <li><a href="lawrence.html">Lawrence</a></li>
+                    <li><a href="gibson.html">Gibson</a></li>
+                    <li><a href="edward.html">Edward</a></li>
+                    <li><a href="meiyee.html">Mei Yee</a></li>
+                    <li><a href="faiq.html">Faiq</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
+    <nav class="nav__wrapper-login">
+        <a class="nav__login-link" href="login.html">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+                </svg>
+            </a>
+            <div class="index__dropdown-login">
+                <a href="login.html">Login</a>
+                <a href="registration.html">Register</a>
+            </div>
+    </nav>
+</header>
