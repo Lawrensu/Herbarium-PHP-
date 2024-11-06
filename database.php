b1f3852f2074ef08c03ddab62c665e76d6b8d4ad
Lawrence Lian <LawrensuLeo@gmail.com>
2024-11-06 15:38:17 +0800

    Prepared the base layout for most of the stuff

--- database.php
+++ database.php
@@ -1,18 +1,32 @@
 <?php 
+    // Database connection
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "Leafly_DB";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password);
-
+    // create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Create database
-    $sql = "CREATE DATABASE IF NOT EXISTS Leafly_DB";
+    // Tables creation
+
+    // Registration/Registered User table
+    
+
+
+    // Enquiry table
+
+
+
+    // Contribution table
+
+
+
+    
 
     mysqli_query($conn, $sql);
     mysqli_close($conn);
