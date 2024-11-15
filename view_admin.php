6419a3eb85e5d25d346c0241fc6742f4d0e10409
xggib <104391407@students.swinburne.edu.my>
2024-11-15 08:54:59 +0800

    Login and View admin

--- view_admin.php
+++ view_admin.php
@@ -12,8 +12,33 @@
 </head>
 
 <body>
-
-
-    
+    <h1>User Management</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Fetch users from the database
+            // Assuming $users is an array of user data
+            foreach ($users as $user) {
+                echo "<tr>";
+                echo "<td>{$user['id']}</td>";
+                echo "<td>{$user['username']}</td>";
+                echo "<td>{$user['email']}</td>";
+                echo "<td>
+                        <a href='edit_user.php?id={$user['id']}'>Edit</a> | 
+                        <a href='delete_user.php?id={$user['id']}'>Delete</a>
+                      </td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
