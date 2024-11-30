588012389f739c900ff79d3b48f1adff9cd9b6e6
Lawrence Lian <LawrensuLeo@gmail.com>
2024-12-01 00:53:12 +0800

    Finished most of the stuff

--- error_registration.php
+++ error_registration.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
 <head>
-    <title>Registration Error</title>
-    <meta charset = "utf-8"/>
-    <meta name = "description" content = "Leafly Registration Form" />
-    <meta name = "keywords"    content = " " />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="images/constant/leaf.png">
+
+    <meta name="author" content="MeiYee">
+    <meta name="description" content="Leafly Registration Error">
+    <meta name="keywords" content="registration, error, Leafly, herbarium, plant preservation">
+
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+
+    <title>Registration Error - Leafly</title>
 </head>
 <body>
-<h1>Registration Error</h1>
-<h3>You have missing/empty fields</h3>
-<p>Please click this <a href = "registration.php">link</a> to return to register page</p>
+    <!-- Including header part -->
+    <?php include 'include/header.php'; ?>
+
+    <main class="error__wrapper container">
+        <div class="error__container">
+            <h1>Registration Error</h1>
+            <h3>You have missing/empty fields</h3>
+            <p>Please click this <a href="registration.php">link</a> to return to the registration page.</p>
+        </div>
+    </main>
+
+    <!-- Footer Part -->
+    <footer>
+        <!-- Include footer part -->
+        <?php include 'include/footer.php'; ?>
+
+        <p class="main-acknowledgement"><strong>Acknowledgement</strong></p>
+        <div class="footer__acknowledgement-wrapper">
+            <a target="_blank" class="footer__acknowledgement-link" href="https://www.youtube.com/watch?v=moIHTT2XK9g">Registration Form</a>
+            <a target="_blank" class="footer__acknowledgement-link" href="https://www.geeksforgeeks.org/gradient-borders/">Gradient Border Guide</a>
+        </div>
+    </footer>
+
+    <?php include 'include/bckToTopBtn.php'; ?>
 </body>
 </html>
